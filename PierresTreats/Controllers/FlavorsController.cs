e54838c289e38328b3555f90646a66611fd97104
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2024-02-16 12:25:16 -0600

    Add Flavors and Treats controllers and views Index and Create

--- PierresTreats/Controllers/FlavorsController.cs
+++ PierresTreats/Controllers/FlavorsController.cs
@@ -1,12 +1,36 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using PierresTreats.Models;
 using System.Collections.Generic;
-
+using System.Threading.Tasks;
+using System.Linq;
+using System;
 namespace PierresTreats.Controllers
 {
   public class FlavorsController : Controller
   {
-    
+    private readonly PierresTreatsContext _db;
+    public FlavorsController(PierresTreatsContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Flavor> model = _db.Flavors.ToList();
+      return View(model);
+    }
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Flavor flavor)
+    {
+      _db.Flavors.Add(flavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
  
   }
 }
--- PierresTreats/Controllers/TreatsController.cs
+++ PierresTreats/Controllers/TreatsController.cs
@@ -1,12 +1,37 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using PierresTreats.Models;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System;
 
 namespace PierresTreats.Controllers
 {
   public class TreatsController : Controller
   {
-
+    private readonly PierresTreatsContext _db;
+    public TreatsController(PierresTreatsContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Treat> model = _db.Treats.ToList();
+      return View(model);
+    }
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Treat treat)
+    {
+      _db.Treats.Add(treat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
  
   }
 }
